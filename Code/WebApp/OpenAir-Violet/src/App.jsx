8d592429424d983af2c02fdf383835a317db6c67
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-07-21 00:34:51 +0100

    Setup ReactJS, ViteJS, Firebase Webapp

--- Code/WebApp/OpenAir-Violet/src/App.jsx
+++ Code/WebApp/OpenAir-Violet/src/App.jsx
@@ -1,6 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './css/App.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARBFtgr3pjHfCQX1MboS80DIut9FxvCls",
+  authDomain: "openair-violet.firebaseapp.com",
+  databaseURL: "https://openair-violet-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "openair-violet",
+  storageBucket: "openair-violet.appspot.com",
+  messagingSenderId: "428155808003",
+  appId: "1:428155808003:web:30a08a4f8d24f9287da136",
+  measurementId: "G-TL860G29X5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   const [count, setCount] = useState(0)
